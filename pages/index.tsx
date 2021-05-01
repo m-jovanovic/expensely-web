84aa966e8cf722256a3cf65e402a5cd30b23ccdd
Milan Jovanović <milannis.995@gmail.com>
2021-05-01 15:05:03 +0200

    style(home): Added gradient to landing page and updated tailwind config colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
 const IndexPage = () => (
-  <div className="flex flex-col items-center justify-center min-h-screen py-2">
+  <div className="flex flex-col items-center text-white justify-center min-h-screen bg-gradient-to-b from-light-blue-900 to-cyan-600">
     <Head>
       <title>Expensely - Simple expense tracking</title>
       <link rel="icon" sizes="32x32" type="image/png" href="/favicon.png" />
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
       <h1 className="text-6xl font-bold">Welcome to Expensely</h1>
 
-      <p className="mt-10 text-4xl font-bold"> Coming soon! </p>
+      <p className="mt-10 text-4xl font-bold"> Coming soon </p>
     </main>
 
     <footer className="flex items-center justify-center w-full h-24 border-t">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      black: colors.black,
+      white: colors.white,
+
+      gray: colors.blueGray,
+      red: colors.red,
+      yellow: colors.amber,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      "light-blue": colors.lightBlue,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
